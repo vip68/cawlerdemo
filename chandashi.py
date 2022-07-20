ad1528917de123741fbbb3823854fd4c19757055
flyskyer <18630195@qq.com>
2022-07-20 20:18:05 +0800

    将结果存储为Excel文件

--- chandashi.py
+++ chandashi.py
@@ -7,16 +7,13 @@ Topic   : 蝉大师接口数据抓取
             https://www.chandashi.com/new/android/keyword?appId=239857072
 """
 
-import json
+import time
 import requests
+from pandas import DataFrame
 
+# 这里的cookies仅作为演示，请自行抓取替换
 cookies = {
-    'Hm_lvt_0cb325d7c4fd9303b6185c4f6cf36e36': '1658037713',
-    'cds_session_id': 'grmrs5nmgulcuoqcvkn4kf65n2',
-    'cds_asm_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.'
-                     'eyJ1aWQiOiIyNzIzNjEiLCJpYXQiOjE2NTgwMzc3MjksImV4cCI6MTY2MDYyOTcyOX0.'
-                     'mk0cWK2CJGV5fS_-JjP8J6OEDdizYxKrLMJuMa-i_6A',
-    'Hm_lpvt_0cb325d7c4fd9303b6185c4f6cf36e36': '1658037731',
+    '...': '...'
 }
 
 headers = {
@@ -42,7 +39,8 @@ data = {
     'country': 'cn',
     'market': 'huawei',
     'page': '1',
-    'pageSize': '100',
+    # 接口未限制最大值取值范围，这里可以任意修改
+    'pageSize': '20000',
     'keyword': '',
     'minRank': '',
     'maxRank': '',
@@ -60,9 +58,55 @@ data = {
 # 请求接口数据
 response = requests.post('https://www.chandashi.com/interf/v1/android/keywordCoverList',
                          cookies=cookies, headers=headers, data=data)
-# 打印请求结果
-print(response.json())
 
-# 将结果保存到文件
-with open('chandashi_result.json', 'w', encoding='utf-8') as f:
-    json.dump(response.json(), f, ensure_ascii=False)
+resp = response.json()
+data_list = resp['data']['list']
+
+data_dict = {
+    "关键词": [],
+    "排名": [],
+    "变动": [],
+    "热度": [],
+    "结果数": [],
+    "关键词ID": [],
+    "更新时间": [],
+    "先前排名": [],
+}
+
+index = 0
+index_dict = {
+    '序号': []
+}
+
+# 通过循环来获取响应数据，并添加到字典中
+for line in data_list:
+    index += 1
+    index_dict['序号'].append(index)
+    data_dict['关键词'].append(line['keyword'])
+    data_dict['排名'].append(line['rank'])
+    data_dict['热度'].append(line['priority'])
+    data_dict['结果数'].append(line['result'])
+    data_dict['关键词ID'].append(line['keywordId'])
+    data_dict['更新时间'].append(line['updateTime'])
+
+    if line['change'] == 99999:
+        data_dict['变动'].append('进榜')
+    elif line['change'] == -99999:
+        data_dict['变动'].append('掉榜')
+    else:
+        data_dict['变动'].append(line['change'])
+
+    if line['prevRank'] == 99999:
+        data_dict['先前排名'].append('进榜')
+    elif line['prevRank'] == -99999:
+        data_dict['先前排名'].append('掉榜')
+    else:
+        data_dict['先前排名'].append(line['prevRank'])
+
+print('正在写入Excel，请稍候......')
+
+filename = '蝉大师抓取结果-%s.xlsx' % time.strftime('%Y%m%d%H%M%S')
+dataForm = DataFrame(data_dict, index=index_dict['序号'])
+dataForm.to_excel(filename)
+
+print('导出成功，文件名：%s' % filename)
